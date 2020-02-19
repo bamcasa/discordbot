f31577e60c85a478d02c0586422f448fc9b41998
bamcasa <combeesang@gmail.com>
2020-02-20 02:36:07 +0900

    리액션

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import discord
 
 client = discord.Client()
-with open("token.txt", "r") as f:
-    token = f.read()
 
 @client.event
 async def on_ready():
@@ -10,12 +8,34 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print(message.content)
-    if "gif" in message.content:
-        print("gif")
-        embed = discord.Embed(color=0x363535)
-        embed.set_image(url=f"https://i.imgur.com/mHl2kpv.gif")
-        await message.channel.send(embed=embed)
-    else:
-        print("png")
-client.run(token)
+    if message.content.startswith("퀴즈"):
+        f = open("quiz.txt","r",encoding = "utf-8")
+        quiz = f.readlines()
+        f.close()
+
+        f = open("test.txt", "w", encoding="utf-8")
+        f.write("quiz.txt")
+        f.close()
+        await message.channel.send(quiz[0])
+
+
+
+@client.event
+async def on_reaction_add(reaction, uesr):
+    f = open("test.txt", "r", encoding="utf-8")
+    quiz1 = f.readlines()
+    f.close()
+    f = open("quiz.txt", "r", encoding="utf-8")
+    quiz = f.readlines()
+    f.close()
+    if str(reaction.emoji) == "⭕":
+        if quiz[1] == "O":
+            await reaction.message.channel.send("나이스")
+        elif quiz[1] == "X":
+            await reaction.message.channel.send("아니야")
+    elif str(reaction.emoji) == "❌":
+        print("X")
+
+
+
+client.run("NjU5NjcyNDQ3MTM3NzQyODU3.Xk1diQ.EThmR6UMflGeWJmm2rLK2k2ac7Q")
